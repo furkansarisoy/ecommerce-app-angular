c5043e754b24ac8a4d05b6b77747ddcc1158d663
furkansarisoy <furkansarisoy@yahoo.com>
2021-05-29 00:20:08 +0300

    auth components created

--- src/app/libs/components/components.module.ts
+++ src/app/libs/components/components.module.ts
@@ -1,28 +1,43 @@
 // Angular
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 // Ng Zorro
 import { NzCarouselModule } from 'ng-zorro-antd/carousel';
 import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 // Components
 import { HeaderComponent } from './header/header.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { CategoriesComponent } from './categories/categories.component';
-
+import { LoginComponent } from './authorization/login/login.component';
+import { RegisterComponent } from './authorization/register/register.component';
+import { RouterModule } from '@angular/router';
+import { ResetPasswordComponent } from './authorization/reset-password/reset-password.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
     HomepageComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    LoginComponent,
+    RegisterComponent,
+    ResetPasswordComponent
   ],
   imports: [
     CommonModule,
     NzCarouselModule,
-    NzCardModule
+    NzCardModule,
+    NzButtonModule,
+    FormsModule,
+    NzInputModule,
+    NzIconModule,
+    RouterModule
   ],
   exports: [
     HeaderComponent
--- src/app/libs/pages/user/user.module.ts
+++ src/app/libs/pages/user/user.module.ts
@@ -9,15 +9,30 @@ import { ComponentsModule } from '../../components/components.module';
 import { UserComponent } from './user.component';
 import { HomepageComponent } from '../../components/homepage/homepage.component';
 import { CategoriesComponent } from '../../components/categories/categories.component';
+import { LoginComponent } from '../../components/authorization/login/login.component';
+import { RegisterComponent } from '../../components/authorization/register/register.component';
+import { ResetPasswordComponent } from '../../components/authorization/reset-password/reset-password.component';
 
 const routes: Routes = [
   {
     path: '', component: UserComponent, children: [
       {
+        path: '', redirectTo: 'homepage'
+      },
+      {
         path: 'homepage', component: HomepageComponent
       },
       {
         path: 'categories', component: CategoriesComponent
+      },
+      {
+        path: 'login', component: LoginComponent
+      },
+      {
+        path: 'register', component: RegisterComponent
+      },
+      {
+        path: 'reset-password', component: ResetPasswordComponent
       }
     ]
   }
