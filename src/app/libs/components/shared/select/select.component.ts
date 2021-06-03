39f4c1a1c7a2248ce9a73212569ecd3fb270ab53
furkansarisoy <furkansarisoy@yahoo.com>
2021-06-04 02:38:36 +0300

    select component created

---
+++ src/app/libs/components/shared/select/select.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+export interface SelectOptions {
+  key: string;
+  value: string;
+}
+
+@Component({
+  selector: 'app-select',
+  templateUrl: './select.component.html',
+  styleUrls: ['./select.component.scss']
+})
+export class SelectComponent implements OnInit {
+
+  @Input() value: string;
+  @Input() options: SelectOptions[];
+
+  @Output() valueChange = new EventEmitter<string>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onValueChange() {
+    this.valueChange.emit(this.value);
+  }
+
+}
